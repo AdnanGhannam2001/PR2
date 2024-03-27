29c3dd1f71e1dbb2d96366f94e158193045ac87c
adnan <AdnanGhannam2001>
2024-03-27 15:59:55 +0300

    Update Package to V1.1.2
    - Update IWriteRepository (Change the Return Value for Some Methods)

--- packages/PR2.Shared/Interfaces/IWriteRepository.cs
+++ packages/PR2.Shared/Interfaces/IWriteRepository.cs
@@ -5,13 +5,13 @@ public interface IWriteRepository<T, TTransaction>
 {
     Task<TTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default);
     
-    Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+    Task<T?> AddAsync(T entity, CancellationToken cancellationToken = default);
 
-    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(T entity, CancellationToken cancellationToken = default);
 
-    Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(T entity, CancellationToken cancellationToken = default);
 
-    Task DeleteRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+    Task<int> DeleteRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
