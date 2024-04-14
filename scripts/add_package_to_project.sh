f58250c475e4573529a8bee8b5358c25a5cd9453
adnan <AdnanGhannam2001>
2024-04-14 18:45:19 +0300

    Update Scripts
    - Removed Old Scripts
    - Add 'collect_packages' Script

--- scripts/add_package_to_project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROJECT=$1
-PACKAGE=$2
-VERSION=$3
-
-dotnet add $PROJECT package $PACKAGE --version $VERSION --source "./store/${PACKAGE,,}"
---
+++ scripts/collect_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO: Make sure that release packages are overriding debug packages
+
+PACKAGES_PATHS=$(find ./packages -name "*.nupkg")
+
+for PACKAGE_PATH in $PACKAGES_PATHS
+do
+    PACKAGE_NAME=$(basename -- $PACKAGE_PATH)
+    cp -v $PACKAGE_PATH "./store/$PACKAGE_NAME"
+done
