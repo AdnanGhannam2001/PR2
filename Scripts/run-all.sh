fd6f299970a843c3f86cfff3b6a4d69ee3010433
adnan <AdnanGhannam2001>
2024-05-19 14:12:13 +0300

    Update Scripts

--- Scripts/run-all.sh
+++ Scripts/run-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 dotnet dev-certs https --clear --trust
-dotnet run --project apps/AuthorizationService/src/AuthorizationServer/ & \
-dotnet run --project apps/SocialMediaService/src/SocialMediaService.WebApi -lp https & \
-dotnet run --project apps/ChatService/src/ChatService.csproj -lp https -p:DefineConstants=NO_RABBIT_MQ
+dotnet run --project Applications/AuthorizationService/src/AuthorizationServer/ & \
+dotnet run --project Applications/SocialMediaService/src/SocialMediaService.WebApi -lp https & \
+dotnet run --project Applications/ChatService/src/ChatService.csproj -lp https -p:DefineConstants=NO_RABBIT_MQ
