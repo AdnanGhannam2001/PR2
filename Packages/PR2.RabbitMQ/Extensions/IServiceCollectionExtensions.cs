008cf94d6af3bf94218352ea236d3be76efd1bd6
AdnanGhannam2001 <adnanghannam21@outlook.com>
2024-10-29 22:18:02 +0300

    [Packages] Update `PR2.RabbitMQ` Package
    Update `AddRabbitMQ` Extension Method

--- Packages/PR2.RabbitMQ/Extensions/IServiceCollectionExtensions.cs
+++ Packages/PR2.RabbitMQ/Extensions/IServiceCollectionExtensions.cs
@@ -6,11 +6,19 @@ namespace PR2.RabbitMQ.Extensions;
 
 public static class IServiceCollectionExtensions
 {
-    public static IServiceCollection AddRabbitMQ(this IServiceCollection services, Assembly assembly)
+    public static IServiceCollection AddRabbitMQ(this IServiceCollection services, Assembly assembly,
+                                                 string host, string username, string password)
         => services.AddMassTransit(config =>
             {
                 config.RegisterConsumersFromAssembly(assembly);
-                config.UsingRabbitMq((context, rmq)
-                    => rmq.ConfigurationReceiveEndpointsFromAssembly(assembly, context));
+                config.UsingRabbitMq((context, rmq) =>
+                {
+                    rmq.ConfigurationReceiveEndpointsFromAssembly(assembly, context);
+                    rmq.Host(host, host =>
+                    {
+                        host.Username(username);
+                        host.Password(password);
+                    });
+                });
             });
 }
